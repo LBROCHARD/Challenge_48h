77b4d0045add8d1d23163d172d98fef652fbc591
Marius BOURSE <marius.bourse@ynov.com>
2023-02-06 18:45:08 +0100

    pas fix layout

--- Front/jaja/src/pages/Layout.js
+++ Front/jaja/src/pages/Layout.js
@@ -1,40 +1,28 @@
-// import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "../styles/layout.css";
 
 const Layout = () => {
     return (
-<<<<<<< HEAD
       <div class="header">
         <div class="title">
           <h1 >JAJA</h1>
         </div>
         <div class="buttonHeader">
-          <form action="/Login">
-          <button> Login </button>
+          <form action="/login" type="submit">
+          <button class="button1" onClick="@"> Login </button>
           </form>
 
-          <form action="/Register">
-          <button> Register </button>
+          <form action="/register">
+          <button class="button1" type="submit"> Register </button>
           </form>
         </div>
         <h1 class="email">Email</h1>
-        <form class="a" action="/Profile">
-        <button> Profile </button>
+        <form class="a" action="/profile">
+        <button class="button1" type="submit"> Profile </button>
         </form>
 
-
-      </div>
-=======
-      <>
-        <h1>LAYOUT</h1>
-        <Link to="/basic">basic</Link>
-        <Link to="/home">home</Link>
-        <Link to="/login">login</Link>
-        <Link to="/register">register</Link>
-
         <Outlet />
-      </>
->>>>>>> db6f6424041ff6bef07f68b3ac1e3ccbce78f54c
+      </div>
     )
   };
   
