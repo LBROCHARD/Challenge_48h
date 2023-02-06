aab78f80206dd8f9bb71a53a2ad41b5c164706c1
Marius BOURSE <marius.bourse@ynov.com>
2023-02-06 18:18:26 +0100

    header

--- Front/jaja/src/index.js
+++ Front/jaja/src/index.js
@@ -2,10 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from 'react';
 import './index.css';
-<<<<<<< HEAD
-=======
 import Login from './pages/login';
->>>>>>> db6f6424041ff6bef07f68b3ac1e3ccbce78f54c
 import NoPage from "./pages/NoPage";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
@@ -18,13 +15,8 @@ export default function Index() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-<<<<<<< HEAD
-          <Route path="home" element={null} />
-          {/* <Route path="app" element={<App/>} /> */}
-=======
           <Route path="home" element={<Home/>} />
           <Route path="login" element={<Login/>} />
->>>>>>> db6f6424041ff6bef07f68b3ac1e3ccbce78f54c
           <Route path="*" element={<NoPage />} />
           <Route path="register" element={<Register/>} />
         </Route>
