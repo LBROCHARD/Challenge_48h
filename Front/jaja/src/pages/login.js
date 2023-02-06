4f0bc53b9694c5d24b89d9091583f319a4823cf2
dorian.martin@i=ynov.comme <dorian.martin@ynov.com>
2023-02-06 15:09:53 +0100

    start

---
+++ Front/jaja/src/pages/login.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Outlet, Link } from "react-router-dom";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
